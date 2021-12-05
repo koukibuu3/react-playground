a9f3a2390ff6f3cdc26030a3bb12d9095a1dd59f
koukibuu3 <koukibuu3@gmail.com>
2021-12-05 23:27:43 +0900

    03-fp/01-advantages

---
+++ src/practice/03-fp/01-advantages/comparison.js
@@ -0,0 +1,18 @@
+// 手続き型
+{
+  const octuples = []
+
+  for (let n = 1; n < 101; n += 1) {
+    if (n % 8 == 0) {
+      octuples.push(n)
+    }
+  }
+  console.log(octuples)
+}
+
+// 関数型
+{
+  const range = (start, end) =>
+    [...new Array(end - start).keys()].map((n) => n + start)
+  console.log(range(1, 101).filter((n) => n % 8 === 0))
+}
