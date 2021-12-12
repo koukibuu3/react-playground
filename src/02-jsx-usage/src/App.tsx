d78e0b7b2889458c198020ee3b945dbd63864ec1
koukibuu3 <koukibuu3@gmail.com>
2021-12-12 17:31:09 +0900

    02-jsx-usage

---
+++ src/02-jsx-usage/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Greets from './components/Greets'
+
+const App: React.FunctionComponent = () => (
+  <div className="App">
+    <Greets name="Patty" times={4}>
+      <span role="img" aria-label="rabbit">
+        🐰
+      </span>
+    </Greets>
+  </div>
+)
+
+export default App
